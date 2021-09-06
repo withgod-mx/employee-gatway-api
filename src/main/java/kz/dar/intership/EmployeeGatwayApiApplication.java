8bfabde8043a583120f0d1e04152491a08b8b68a
Yerkebulan <yeyessenov@dar.kz>
2021-09-06 18:08:53 +0600

    first commit

---
+++ src/main/java/kz/dar/intership/EmployeeGatwayApiApplication.java
@@ -0,0 +1,13 @@
+package kz.dar.intership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeGatwayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeGatwayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/dar/intership/EmployeeGatwayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.dar.intership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeGatwayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
